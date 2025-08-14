e5ceecd2c807a0553800f9e5f3b6d1f136f8df17
Aashraya Basva <aashraya.basva@gmail.com>
2025-08-14 22:00:30 +0530

    Initial Commit

---
+++ Python/Day 6/main.py
@@ -0,0 +1,26 @@
+a = 5
+print(a)
+b = "hi"
+c = 5.5
+d= True
+e= None
+f =4 + 3j
+g = 'hello'
+h = [1, 2, 3]
+i = ["hello", 1, 2.5]
+j =(1, 2, 3)
+k = {
+    a: "one",
+    b: "two",
+}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
---
+++ Python/Day 8/main.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a number: "))
+b = input("Enter operator: ")
+c = int(input("Enter another number: "))
+
+if(b == '+'):
+    print(a + c)
+elif(b == '-'):
+    print(a - c)    
+elif(b == '*'):
+    print(a * c)        
+elif(b == '/'):
+    print(a / c)
+
