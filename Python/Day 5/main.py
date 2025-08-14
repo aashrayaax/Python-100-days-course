b086a9404548060e3d4917246159da50e85d8329
Aashraya Basva <aashraya.basva@gmail.com>
2025-08-14 14:36:40 +0530

    Initial Commit

---
+++ Python/Day 5/main.py
@@ -0,0 +1,25 @@
+# hi iam a comment
+# im single line comment
+
+# im multi line comment
+# canu see me
+
+
+'''we can write comments like this also'''
+
+"""and like this also"""
+
+print("hello aashraya\ngood morning")  
+# used for new line 
+
+print("HI MY NAME IS AASHRAYA I AM \"20\" YEARS OLD I AM FROM INDIA")
+print("HI MY NAME IS AASHRAYA I AM \'20\' YEARS OLD I AM FROM INDIA")
+
+print(7)
+print("hey",9,8)
+print("hey",9,8,sep="*")  # sep is used to separate the values with a character
+print("hey",9,8,sep="*",end="1245")  # end is used
+
+
+print("BYE BYE \n END OF THE SESSION")  # \N is used to print a new line
+print("BYE BYE \n END OF THE SESSION",end="!")  # end
