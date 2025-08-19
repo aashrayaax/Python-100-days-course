70665e0ccab0b29c6142d931f34fdddb46bb6e5b
Aashraya Basva <aashraya.basva@gmail.com>
2025-08-19 18:41:46 +0530

    Initial Commit

---
+++ Python/Day 11/main.py
@@ -0,0 +1,36 @@
+name = "Aashraya"
+friend = "Diana"
+anotherFriend = "Jaya"
+print(name)
+
+content = print('"Hi This ia content"')
+content1 = print("'Hi This ia content'")
+content2 = print("\"Hi This ia content\"")
+
+matter = '''
+Git is a version control system that tracks changes in code. GitHub is a platform where developers store and collaborate on projects. Key steps include creating a repository, committing changes, and pushing updates. It enables teamwork, code review, and project management, making software development more efficient and organized
+'''
+
+print(matter)
+
+st = """Hello, Harry
+
+Note: It does not matter whether you enclose your strings in single or double quotes, the output remains the same.
+
+Sometimes, the user might need to put quotation marks in between the strings. Example, consider the sentence: He said, “I want to eat an apple”.
+
+How will you print this statement in python?: He said, "I want to eat an apple". We will definitely use single quotes for our convenience"""
+
+print(st)
+
+print(name[0])
+print(name[1])
+
+print(name[2])
+print(name[3])      
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+# print(name[9])
