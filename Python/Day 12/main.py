acb15ded97e21270c047dc5088b6d205b2eb42dc
Aashraya Basva <aashraya.basva@gmail.com>
2025-08-28 16:29:17 +0530

    Initial Commit

---
+++ Python/Day 12/main.py
@@ -0,0 +1,17 @@
+names = "Harry,Shubham"
+print(names[1:4])
+
+first = "Harry"
+length =    len(first)
+print("The length of first is ",length)
+
+second = "Shubham"
+print(second[0:4])
+print(second[:4])
+print(second[4:])
+print(second[0:])
+print(second[:])
+print(second[1:-2])
+print(second[-4:-1])
+# this is all about slicing in python
+
