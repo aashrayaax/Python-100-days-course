d44548c7cc5e52db71ddb9b899e9ae9a44d55793
Aashraya Basva <aashraya.basva@gmail.com>
2025-08-28 19:17:47 +0530

    Initial Commit

---
+++ Python/Day 13/main.py
@@ -0,0 +1,47 @@
+a = "aashraya"
+print(len(a))
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a)
+a1 = "HelloGuys"
+print(a1.strip("Hello"))
+print(a1.replace("Guys","Friends"))
+print(a1.split(" "))
+print(a1.center(50))
+print(a1.count("u"))
+print(a1.startswith("H"))
+print(a1.startswith("g"))
+print(a1.endswith("h"))
+
+print(a1.endswith("s"))
+
+str = "HelloHelloMiketesting1456"
+print(str.find("Mike"))
+# print(str.index("Mikej"))
+print(str.isalnum())
+
+str2 = "gfdhfgkuh"
+print(str2.isalpha())
+
+# text = "Hello"
+# print(text.isalpha())  # True
+
+# text2 = "Hello123"
+# print(text2.isalpha())  # False
+
+
+str3 = "AAaashu"
+print(str3.islower())
+
+str4 = "Aashu Is Good "
+print(str4.isupper())
+print(str4.isprintable())
+print(str4.swapcase())
+print(str4.title())
+print(str4.istitle())
+
+str5 = "    vgjcs   "
+print(str5.isspace())
+
+
