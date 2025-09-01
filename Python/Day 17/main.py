073c6307ed509ff8f0462ac8022b1fc9883f365a
Aashraya Basva <aashraya.basva@gmail.com>
2025-09-01 20:13:56 +0530

    Initial Commit

---
+++ Python/Day 17/main.py
@@ -0,0 +1,23 @@
+# name = "Aashraya"
+# for i in name:
+#     print(i)
+
+# List = ["Red","Green","Yellow","Pink"]
+# for j in List:
+#     print(j)
+#     for k in j:
+#         print(k)
+
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,2001):
+#     print(i)
+
+for j in range(1,25,5):
+    print(j-1)
+
+
+
+
