08cb65e37b717df670eaad8156647ea0c5c86310
Aashraya Basva <aashraya.basva@gmail.com>
2025-09-04 21:10:32 +0530

    Initial Commit

---
+++ Python/Day 18/main.py
@@ -0,0 +1,25 @@
+# while{
+#     print()
+# }condition
+
+i = 0
+while(i<=5):
+    print(i)
+    i = i + 1
+
+
+
+
+for i in range(6):
+    print(i)
+
+print("separator")
+# do while eg    
+# i = int(input("Enter the number: "))
+i = 22
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with the loop")
