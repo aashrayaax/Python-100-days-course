c2aa4a26f7f17706cbb8ec9d6f02d5665c299774
Aashraya Basva <aashraya.basva@gmail.com>
2025-09-05 20:55:43 +0530

    Initial Commit

---
+++ Python/Day 20/main.py
@@ -0,0 +1,25 @@
+a = 2
+b = 9
+gmean = (a*b)/(a+b)
+print(gmean)
+
+c = 8
+d = 6
+gmean1 = (c*d)/(c+d)
+print(gmean1)
+
+
+def GeoMean(x,y):
+    h = (x*y)/(x+y)
+    print(h)
+GeoMean(15,562)    
+GeoMean(45,8923)
+
+
+# to print the greatest outn of 2 
+def add(u,v):
+    if(u>v):
+        print(" u is the greatest number")
+    else:
+        print(" v is the greatest number")
+add(120,5)            
